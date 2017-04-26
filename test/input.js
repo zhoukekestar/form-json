b41f354947908991b2a71fa412576523f16f0849
keke <zhoukekestar@163.com>
2017-04-26 14:22:41 +0800

    update should to assert

--- test/input.js
+++ test/input.js
@@ -1,48 +1,63 @@
+var assert = chai.assert;
 describe('Input', () => {
 
   describe('a', () => {
-    it(' should equal to object', () => {
+    it(' should equal to object', (done) => {
       var form = createForm('<input name="a" value="b">');
       form.beforeStringify = function(d) {
-        d.a.should.equal("b");
+        assert.deepEqual(d, {a: "b"});
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe('a.', () => {
-    it(' should equal to array', () => {
+    it(' should equal to array', (done) => {
       var form = createForm('<input name="a." value="b">');
       form.beforeStringify = function(d) {
-        d.a[0].should.equal("b");
-        d.a.length.should.equal(1);
+        assert.deepEqual(d, {a: ["b"]});
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe('a.b', () => {
-    it(' should equal to an nested object', () => {
+    it(' should equal to an nested object', (done) => {
       var form = createForm('<input name="a.b" value="c">');
       form.beforeStringify = function(d) {
-        d.a.b.should.equal("c");
+        assert.deepEqual(d, {a: {b: "c"}});
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe('a.NUNBER.b', () => {
-    it(' should equal to an complex array', () => {
+    it(' should equal to an complex array', (done) => {
       var form = createForm(`
         <input name="a.1.a" value="a1a" />
         <input name="a.6.a" value="a6a" />
         <input name="a.6.b" value="a6b" />
         `);
       form.beforeStringify = function(d) {
-        d.a.length.should.equal(2);
-        d.a[0].a.should.equal('a1a');
-        d.a[1].a.should.equal('a6a');
-        d.a[1].b.should.equal('a6b');
+        assert.deepEqual(d, {
+          a: [
+            {
+              a: 'a1a',
+            },
+            {
+              a: 'a6a',
+              b: 'a6b'
+            }
+          ]
+        });
+        form.remove();
+        done();
       }
       form.submit();
     })
--- test/type.js
+++ test/type.js
@@ -1,85 +1,104 @@
+var assert = chai.assert;
 describe('Type', () => {
 
   describe(':string', () => {
-    it(' should equal to string', () => {
+    it(' should equal to string', (done) => {
       var form = createForm('<input name="a" value="b">');
       form.beforeStringify = function(d) {
-        d.a.should.equal("b");
+        assert.strictEqual(d.a, "b");
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe(':bool', () => {
-    it(' should equal to true', () => {
+    it(' should equal to true', (done) => {
       var form = createForm('<input name="a:bool" value="true">');
       form.beforeStringify = (d) => {
-        d.a.should.equal(true);
+        assert.strictEqual(d.a, true);
+        form.remove();
+        done();
       }
       form.submit();
     })
-    it(' should equal to false', () => {
+    it(' should equal to false', (done) => {
       var form = createForm('<input name="a:bool" value="false">');
       form.beforeStringify = (d) => {
-        d.a.should.equal(false);
+        assert.strictEqual(d.a, false);
+        form.remove();
+        done();
       }
       form.submit();
     })
-    it(' should equal to false', () => {
+    it(' should equal to false', (done) => {
       var form = createForm('<input name="a:bool" value="0">');
       form.beforeStringify = (d) => {
-        d.a.should.equal(false);
+        assert.strictEqual(d.a, false);
+        form.remove();
+        done();
       }
       form.submit();
     })
-    it(' should equal to true', () => {
+    it(' should equal to true', (done) => {
       var form = createForm('<input name="a:bool" value="1">');
       form.beforeStringify = (d) => {
-        d.a.should.equal(true);
+        assert.strictEqual(d.a, true);
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe(':number', () => {
-    it(' should equal to 10', () => {
+    it(' should equal to 10', (done) => {
       var form = createForm('<input name="a:number" value="10">');
       form.beforeStringify = function(d) {
-        d.a.should.equal(10)
+        assert.strictEqual(d.a, 10)
+        form.remove();
+        done();
       }
       form.submit();
     })
-    it(' should equal to 0.1', () => {
+    it(' should equal to 0.1', (done) => {
       var form = createForm('<input name="a:number" value="0.1">');
       form.beforeStringify = function(d) {
-        d.a.should.equal(0.1)
+        assert.strictEqual(d.a, 0.1)
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe(':object', () => {
-    it(' should equal to object', () => {
+    it(' should equal to object', (done) => {
       var form = createForm(`<input name="a:object" value='{"a":"a"}'>`);
       form.beforeStringify = function(d) {
-        d.a.a.should.equal('a');
+        assert.deepEqual(d, {a: {a :'a'}});
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe(':timestamp', () => {
-    it(' should equal to timestamp', () => {
+    it(' should equal to timestamp', (done) => {
       var form = createForm(`<input name="a:timestamp" value='2000-01-01'>`);
       form.beforeStringify = function(d) {
-        d.a.should.equal(946684800000);
+        assert.strictEqual(d.a, 946684800000);
+        form.remove();
+        done();
       }
       form.submit();
     })
   })
 
   describe(':custom', () => {
-    it(' should equal to ISOString', () => {
+    it(' should equal to ISOString', (done) => {
 
       HTMLFormJSONElement.registerType('ISOString', function (value) {
         return new Date(value).toISOString();
@@ -87,7 +106,9 @@ describe('Type', () => {
 
       var form = createForm(`<input name="a:ISOString" value='Fri, 31 Mar 2017 07:00:00'>`);
       form.beforeStringify = function(d) {
-        d.a.should.equal('2017-03-30T23:00:00.000Z');
+        assert.strictEqual(d.a, '2017-03-30T23:00:00.000Z');
+        form.remove();
+        done();
       }
       form.submit();
     })
